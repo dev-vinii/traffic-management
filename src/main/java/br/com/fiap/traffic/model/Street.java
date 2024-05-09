40c42674a6505ce068243c73c1a328568200a82f
ViniciusLuna <viniciusluna@yahoo.com>
2024-05-08 23:19:55 -0300

    creating street model

--- src/main/java/br/com/fiap/traffic/model/Street.java
+++ src/main/java/br/com/fiap/traffic/model/Street.java
@@ -1,7 +1,16 @@
 package br.com.fiap.traffic.model;
 
-import java.util.Objects;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
 
+@Entity
+@Table(name = "tbl_street")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class Street {
 
     private Long id;
